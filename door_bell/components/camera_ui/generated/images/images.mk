c7daf3da9dc785fbe231f377ebde7100f1814359
du_jianmin <20130527@qq.com>
2024-03-17 19:45:54 +0800

    remove door-dell

--- door_bell/components/camera_ui/generated/images/images.mk
+++
@@ -1,8 +0,0 @@
-## SPDX-License-Identifier: MIT
-## Copyright 2020-2023 NXP
-GEN_CSRCS += $(notdir $(wildcard $(PRJ_DIR)/generated/images/*.c))
-
-DEPPATH += --dep-path $(PRJ_DIR)/generated/images
-VPATH += :$(PRJ_DIR)/generated/images
-
-CFLAGS += "-I$(PRJ_DIR)/generated/images"
